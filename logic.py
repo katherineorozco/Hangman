0ee16cba453cd947af25faef46d66eef7d194cb3
katherineorozco <150992954+katherineorozco@users.noreply.github.com>
2023-12-05 21:43:29 -0600

    Check letter is in word

--- logic.py
+++ logic.py
@@ -21,8 +21,6 @@ class Logic(QMainWindow, Ui_Hangman):
         self.read_data()
         self.new_word()
         self.guessed_letter = self.input_letter.text().strip()
-        print(self.guessed_letter)
-
 
     def read_data(self):
         filename = 'words.txt'
@@ -35,23 +33,52 @@ class Logic(QMainWindow, Ui_Hangman):
     def check_word(self):
         if self.max_attempts > 0:
             if self.guessed_letter.isalpha():
+                word_display = ''
+                letter_found = False
                 for letter in self.word_to_guess:
-                    if self.guessed_letter == self.word_to_guess:
-                        pass
+                    if self.guessed_letter == letter:
+                        word_display += letter
+                        letter_found = True
+                    else:
+                        word_display += "_"
+
+                    self.label_word.setText(word_display)
+
+                    if not letter_found:
+                        self.max_attempts -= 1
+                        self.show_body()
+                        self.label_result.setText(f"Incorrect! {self.max_attempts} attempts remaining.")
 
             else:
                 self.max_attempts -= 1
+                self.show_body()
                 self.label_result.setText(f"Incorrect! {self.max_attempts} attempts remaining.")
 
         else:
             self.new_word()
-            self.label_result.setText("You failed to guess the word :(")
+            self.label_result.setText(f"Wrong! It was {self.word_to_guess}. :( \nClick New Word!")
             return None
 
+    def show_body(self):
+        if self.max_attempts == 5:
+            self.man_head.setVisible(True)
+        if self.max_attempts == 4:
+            self.man_body.setVisible(True)
+        if self.max_attempts == 3:
+            self.man_leftarm.setVisible(True)
+        if self.max_attempts == 2:
+            self.man_rightarm.setVisible(True)
+        if self.max_attempts == 1:
+            self.man_leftleg.setVisible(True)
+        if self.max_attempts == 0:
+            self.man_rightleg.setVisible(True)
+
     def submit(self):
-        pass
+        self.check_word()
+        self.input_letter.clear()
 
     def new_word(self):
+        self.max_attempts = 6
         self.input_letter.clear()
         self.label_result.clear()
         self.word_to_guess = random.choice(self.word_list).lower()
